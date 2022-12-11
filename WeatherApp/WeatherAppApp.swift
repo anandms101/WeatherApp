7823167b3d2585c22c9a52348d01ba112f37f980
ANAND MOHAN SINGH <anandmohan.singh101@gmail.com>
2022-12-11 21:27:02 +0530

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Anand Mohan Singh on 11/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
